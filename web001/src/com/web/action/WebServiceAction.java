fcb91ed0a89ef9fc533928880d34e02ab68b8fa5
liupeng <13405006821@139.com>
2015-07-23 13:51:59 +0800

    1

--- web001/src/com/web/action/WebServiceAction.java
+++ web001/src/com/web/action/WebServiceAction.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.base.action.BaseAction;
 import com.base.util.Constants;
-import com.base.util.Json;
 import com.web.entity.Question;
 import com.web.service.IQuestionService;
 
@@ -35,15 +34,8 @@ public class WebServiceAction extends BaseAction {
 	 */
 	@SuppressWarnings("unchecked")
 	public void getAllQuestion() throws Exception {
-		Json json = new Json();
-		try {
-			List<Question> list = (List<Question>) getServletContext().getAttribute(Constants.APPLICATION_ALL_QUESTION);
-			json.setSuccess(true);
-			json.setObj(list);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		writeJson(json);
+		List<Question> list = (List<Question>) getServletContext().getAttribute(Constants.APPLICATION_ALL_QUESTION);
+		writeJson(list);
 	}
 	
 	/**
@@ -52,22 +44,15 @@ public class WebServiceAction extends BaseAction {
 	 */
 	@SuppressWarnings("unchecked")
 	public void getTestQuestion() throws Exception {
-		Json json = new Json();
-		try {
-			List<Question> list = (List<Question>) getServletContext().getAttribute(Constants.APPLICATION_ALL_QUESTION);
-			Set<Integer> set = new HashSet<Integer>();
-			while(set.size() < 100){
-				set.add((int) ((list.size() - 1) * Math.random()));
-			}
-			List<Question> result = new ArrayList<Question>();
-			for(int i : set){
-				result.add(list.get(i));
-			}
-			json.setSuccess(true);
-			json.setObj(result);
-		} catch (Exception e) {
-			e.printStackTrace();
+		List<Question> list = (List<Question>) getServletContext().getAttribute(Constants.APPLICATION_ALL_QUESTION);
+		Set<Integer> set = new HashSet<Integer>();
+		while(set.size() < 100){
+			set.add((int) ((list.size() - 1) * Math.random()));
 		}
-		writeJson(json);
+		List<Question> result = new ArrayList<Question>();
+		for(int i : set){
+			result.add(list.get(i));
+		}
+		writeJson(result);
 	}
 }
