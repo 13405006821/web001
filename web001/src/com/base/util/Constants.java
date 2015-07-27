7af495a83a3d30b9cf9c009dccf99c11f42ed0ea
liupeng <13405006821@139.com>
2015-07-27 20:55:02 +0800

    1

--- web001/src/com/base/util/Constants.java
+++ web001/src/com/base/util/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 	
 	public static final String SESSION_USER_ID = "SESSION_USER_ID";
 	
-	public static final String APPLICATION_ONLINE_USER = "APPLICATION_ONLINE_USER";
+	public static final String APPLICATION_COMPANYNAME = "APPLICATION_COMPANYNAME";
+	public static final String APPLICATION_SYSTEMNAME = "APPLICATION_SYSTEMNAME";
 }
--- web001/src/com/web/listener/SystemInitialListener.java
+++ web001/src/com/web/listener/SystemInitialListener.java
@@ -1,9 +1,14 @@
 package com.web.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.apache.log4j.Logger;
+
+import com.base.util.Constants;
+import com.base.util.SpringUtil;
+import com.base.util.SystemConfig;
 
 /**
  * 加载系统配置文件
@@ -22,5 +27,9 @@ public class SystemInitialListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent config) {
+		ServletContext application = config.getServletContext();
+		SystemConfig systemConfig = (SystemConfig) SpringUtil.getBean("systemConfig");
+		application.setAttribute(Constants.APPLICATION_COMPANYNAME, systemConfig.getCompanyName());
+		application.setAttribute(Constants.APPLICATION_SYSTEMNAME, systemConfig.getSystemName());
 	}
 }
